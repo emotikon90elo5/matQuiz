e3a856772d8f7670a92f54d06b8bb3a89152735e
kubi2392 <kuba3455.gimajl.kd@wp.pl>
2023-01-01 01:45:13 +0100

    updating login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,6 +4,23 @@
 <link rel="stylesheet" href="{{asset('css/login.css')}}">
 <div id="loginbox">
     <div id="logininnerbox">
+        <div id="logininpu">
+            <div class="input">
+                <input type="text" id="login" name="login" value="">
+                <label for="login"> login </label>
+            </div>
+        </div>
+        <div id="passwordinpu">
+            <div class="input">
+                <input type="password" id="password" name="pasword" value="">
+                <label for="password"> password </label>
+            </div>
+        </div>
+        <div class="buttons">
+        <button id="loginbut">Zalogój</button>
+        <button id="registrbut">Rejestrój</button><br>
+        <button id="paswordresetbut">Rejestrój<br> hasło</button>
+        </div>
     </div>
 </div>
 
