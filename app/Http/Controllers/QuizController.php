1fe573b0f8c2a1085f1008ab0d256797d9389bdf
kubi2392 <kuba3455.gimajl.kd@wp.pl>
2023-01-02 21:59:45 +0100

    A

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class QuizController extends Controller
 {
     public function show(){
-        return view("user.question");
+        return view("quiz.question");
     }
 
     //
