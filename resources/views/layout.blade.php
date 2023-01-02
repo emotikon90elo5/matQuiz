4cd22ed53f4d7baa411a8d7f7a49018d75ecd138
kubi2392 <kuba3455.gimajl.kd@wp.pl>
2023-01-02 21:30:20 +0100

    A

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -50,12 +50,12 @@
         <div class="nav">
             <div class="dropdown">
                 <button class="dropbtn">Dropdown</button>
-            <div class="dropdown-content">
-                    <a href="#">Link 1</a>
-                    <a href="#">Link 2</a>
-                    <a href="#">Link 3</a>
+                <div class="dropdown-content">
+                        <a href="#">Link 1</a>
+                        <a href="#">Link 2</a>
+                        <a href="#">Link 3</a>
+                </div>
             </div>
-        </div>
         </div>
         <div>
             <div class="socialbg">
