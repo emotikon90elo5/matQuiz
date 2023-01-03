9c46dc0066684abd160726702f4f2e9ff2c30a38
kubi2392 <kuba3455.gimajl.kd@wp.pl>
2023-01-03 22:17:55 +0100

    question

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,46 @@
-@extends("layout")
+@extends('layout')
 
-@section("content")
+@section('content')
+    <link rel="stylesheet" href="{{ asset('css/auth/login.css') }}">
 
-<link rel="stylesheet" href="{{asset('css/auth/login.css')}}">
+    <div id="loginbox">
 
-<div id="loginbox">
+        <div id="logininnerbox">
 
-    <div id="logininnerbox">
-
-        <div id="logininpu">
-            <div class="input">
-                <input type="text" id="login" name="login" value="">
-                <label for="login"> login </label>
-                <div class="error">
-                    <p id="errorlogin">
-                        @error('record')
-                        @enderror
-                    </p>
+            <div id="logininpu">
+                <div class="input">
+                    <input type="text" id="login" name="login" value="">
+                    <label for="login"> login </label>
+                    <div class="error">
+                        <p id="errorlogin">
+                            @error('record')
+                            @enderror
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div id="passwordinpu">
-            <div class="input">
-                <input type="password" id="password" name="pasword" value="">
-                <label for="password"> password </label>
-                <div class="error">
-                    <p id="errorpassword">
-                        @error('record')
-                        @enderror
-                    </p>
+            <div id="passwordinpu">
+                <div class="input">
+                    <input type="password" id="password" name="pasword" value="">
+                    <label for="password"> password </label>
+                    <div class="error">
+                        <p id="errorpassword">
+                            @error('record')
+                            @enderror
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="buttons">
-            <button id="loginbut">Zalogój</button>
-            <button id="registrbut">Rejestrój</button><br>
-            <button id="paswordresetbut">Rejestrój<br> hasło</button>
+            <div class="buttons">
+                <button id="loginbut">Zalogój</button>
+                <button id="registrbut">Rejestrój</button><br>
+                <button id="paswordresetbut">Rejestrój<br> hasło</button>
+            </div>
+
         </div>
 
     </div>
-
-</div>
-
 @endsection
+
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,21 +1,23 @@
 <!DOCTYPE html>
 <html lang="pl">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('css/global.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/global.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-    @yield("head")
+    @yield('head')
 </head>
+
 <body>
     <nav>
         <div class="logo">
             <a href="/">
-                <img id="img" src="{{asset('logo.svg')}}" alt="no img">
-            </a>
+                <img id="img" src="{{ asset('logo.svg') }}" alt="no img">
+            </a>+
         </div>
         <div class="nav">
             <div class="dropdown">
@@ -51,21 +53,23 @@
             <div class="dropdown">
                 <button class="dropbtn">Dropdown</button>
                 <div class="dropdown-content">
-                        <a href="#">Link 1</a>
-                        <a href="#">Link 2</a>
-                        <a href="#">Link 3</a>
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
                 </div>
             </div>
         </div>
         <div>
             <div class="socialbg">
-            <a href="https://github.com/emotikon90elo5">
-                <img class="social" src="{{asset('github.png')}}" alt="no img">
-            </a>
-            <div>
-        </div>
+                <a href="https://github.com/emotikon90elo5">
+                    <img class="social" src="{{ asset('github.png') }}" alt="no img">
+                </a>
+                <div>
+                </div>
     </nav>
-    <article>@yield("content")</article>
+    <article>@yield('content')</article>
     <footer>footer</footer>
 </body>
+
 </html>
+
